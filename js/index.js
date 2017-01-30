54296951f12bd8f03a3d9d9619925807b2cbdb89
Rage Hills <1207581brunel@gmail.com>
2017-01-30 16:25:56 +0000

    keydown function needs altering

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-var e = .Event("keyup");
+// var e = jQuery.Event("keyup");
+// e.which = 38; // up key
+// // e.which = 40; // down key
+// $("#playermd").css("background-color", "white");
+// $("#playermu").css("background-color", "white");
+// $("#playerm").css("background-color", "black");
+// });
 
-e.which = 38; // up key
-e.which = 40; // down key
-$("id_to_element").trigger(e);
+var moveUp = document.getElementById("playermu");
+var moveDown = document.getElementById("playermd");
+var originalP = document.getElementById("playerm");
+var keyDown = Event.keyCode()
+
+	if (keyDown == 38) {
+	    $("#playermu").css("background-color", "white")
+	} else if (keyDown == 40) {
+	    $("#playermd").css("background-color", "white")
+	}
+});
