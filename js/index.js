29792fa4ccd1d1c4e525595fefb5582ac0335c6a
Rage Hills <1207581brunel@gmail.com>
2017-02-01 11:21:41 +0000

    code master fixing of js layout done

--- js/index.js
+++ js/index.js
@@ -1,98 +1,75 @@
 $(document).ready(function(){
 	$( ".gamePage" ).hide();
 	$( ".gameLoss" ).hide();
-	$("#startGameB").click(function() {
-  		$(".startGame").slideUp();
+	var playerMove = 'middle';
+	var score = 0;
+	var timer;
+	$("#startGameB").click(startGame);
+	$("#replayGameB").click(function() {
+		document.location.reload();
+	});
+
+	function startGame() {
+		$(".startGame").slideUp();
   		$(".gamePage").show();
 
-  		var timeD = 0;
-  		var timeU = 0;
-		var start = $(".row#2").offset().left;
-		var playerMove = 'middle'
-		var finish = $("#g8").offset().left;
-		var score = 0
 
 		$(document).on("keydown", backgroundChange)
+		$(document).on("keyup", backgroundChangeBack)
 		$("#playerm").css("background-color", "white")
 
-		// players move
-		function backgroundChange(event) {
-			// console.log("3")
-			if (event.keyCode === 38) {
-		    	$("#playermu").css("background-color", "white")
-		    	$("#playerm").css("background-color", "transparent")
-		    	playerMove = 'up'
-		    	timeD = new Date().getTime();
-		    	// console.log(timeD)
-		    	// return timeD;
-		    } else if (event.keyCode === 40) {
-		    	$("#playermd").css("background-color", "white")
-		    	$("#playerm").css("background-color", "transparent")
-		    	playerMove = 'down'
-		    	timeD = new Date().getTime();
-		    	// return timeD;
-		    } else {
-		    	$("#playermu").css("background-color", "transparent")
-		    	$("#playerm").css("background-color", "white")
-		    	playerMove = 'middle'
-		    }
-		};
-
-		// players move back
-		$(document).on("keyup", backgroundChangeBack)
-		function backgroundChangeBack(event) {
-			// console.log("4")
-			if (event.keyCode === 38) {
-				$("#playerm").css("background-color", "white")
-				$("#playermu").css("background-color", "transparent")
-				playerMove = 'middle'
-				timeU = new Date().getTime();
-		    	// return timeu;
-		    } else if (event.keyCode === 40) {
-		    	$("#playerm").css("background-color", "white")
-		    	$("#playermd").css("background-color", "transparent")
-		    	playerMove = 'middle'
-		    	timeU = new Date().getTime();
-		    	// return timeU;
-		    } else {
-				$("#playermd").css("background-color", "none")
-				playerMove = 'middle'
-		    }
-		};
-
-		function compareTime () {
-			if ((timeU).val - (timeD).val > 500);
-				$(".gamePage").hide(1000);
-	  			$(".gameLoss").show();
-	  			$('.scoreBoard').text(score);
-		};
-
 		// Bullet animation & refire
-		$(document).on("click", function() {
-			
-			setInterval(function() {
-				$(".bullet").css({left: start}).animate({left: finish}, 1000);
+		$(document).on("click", fireBullet);
+	}
 
-				setTimeout(function() {
-					// console.log("made it to the person");
-					if (playerMove !== 'middle') {
-						score++;
-						// console.log(score);
-						$('#score').text(score);
-					// } else if (time >= 501) {
-					// 	$(".gamePage").hide(1000);
-	  				//	$(".gameLoss").show();
-	  				// 	$('.scoreBoard').text(score);
-					} else {
-						$(".gamePage").hide(1000);
-	  					$(".gameLoss").show();
-	  					$('.scoreBoard').text(score);
-					}
-				}, 850);
-			}, 2000);
-		});
-		$("#replayGameB").click(function() {
-			document.location.reload();
-		});
-	});
+	function backgroundChange(event) {
+		timer = setTimeout(endGame, 500);
+		if (event.keyCode === 38) {
+	    	$("#playermu").css("background-color", "white")
+	    	$("#playerm").css("background-color", "transparent")
+	    	playerMove = 'up'
+	    } else if (event.keyCode === 40) {
+	    	$("#playermd").css("background-color", "white")
+	    	$("#playerm").css("background-color", "transparent")
+	    	playerMove = 'down'
+	    } else {
+	    	$("#playermu").css("background-color", "transparent")
+	    	$("#playerm").css("background-color", "white")
+	    	playerMove = 'middle'
+	    }
+	};
+
+			// players move back
+	function backgroundChangeBack() {
+		clearTimeout(timer);
+		playerMove = 'middle';
+		$("#playerm").css("background-color", "white");
+	    $("#playermd").css("background-color", "transparent");
+		$("#playermu").css("background-color", "transparent");
+	};
+
+	function fireBullet() {
+		var start = $(".row#2").offset().left;
+		var finish = $("#g8").offset().left;
+		setInterval(function() {
+			$(".bullet").css({left: start}).animate({left: finish}, 1000);
+
+			setTimeout(function() {
+				if (playerMove !== 'middle') {
+					score++;
+					$('#score').text(score);
+				} else {
+					endGame();
+				}
+			}, 850);
+		}, 2000);
+	};
+
+	function endGame() {
+		$(".gamePage").hide(1000);
+  		$(".gameLoss").show();
+  		$('.scoreBoard').text(score);
+	}
+
+
 });
