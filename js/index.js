5a6e7531155f98aa92be3914ebdc36e7d3427922
Rage Hills <1207581brunel@gmail.com>
2017-02-01 22:27:56 +0000

    player sprite adjusted

--- js/index.js
+++ js/index.js
@@ -57,27 +57,24 @@ $(document).ready(function(){
 	function fireBullet() {
 		var start = $(".row#2").offset().left;
 		var finish = $("#g8").offset().left;
-		var rand = ((Math.round(Math.random()*(400)*10) + 200));
-		var check = rand - 50
 		setInterval(function() {
-			console.log(check);
+			var rand = ((Math.round(Math.random()*(400)*8) + 200));
 			console.log(rand);
 			$(".bullet").css({left: start}).animate({left: finish}, 1000);
 
-			setTimeout(bulletCheck, check);
-				function bulletCheck() {
-					if (playerMove !== 'middle') {
-						score++;
-						$('#score').text(score);
-					} else {
-						$("#dizzy").show()
-						$("#idle").hide()
-						$("#playermd").hide()
-						$("#playermu").hide()
-						endGame();
-					}
-				};
-		}, rand);
+			setTimeout(function() {
+				if (playerMove !== 'middle') {
+					score++;
+					$('#score').text(score);
+				} else {
+					$("#dizzy").show()
+					$("#idle").hide()
+					$("#playermd").hide()
+					$("#playermu").hide()
+					endGame();
+				}
+			}, 800);
+		}, 2000);
 	};
 
 	function endGame() {
